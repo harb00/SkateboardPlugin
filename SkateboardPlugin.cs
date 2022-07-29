e1f79d6629d704b31314ef12b71e90654748f609
harb00 <110241339+harb00@users.noreply.github.com>
2022-07-29 15:57:12 +0200

    initial commit

---
+++ SkateboardPlugin.cs
@@ -0,0 +1,41 @@
+﻿using Plus.Plugins;
+using Plus.HabboHotel.Rooms.Chat.Commands;
+using Plus.HabboHotel.GameClients;
+using Plus.HabboHotel.Rooms;
+
+namespace SkateboardPlugin;
+
+public class SkateboardPlugin : IPlugin
+{
+    
+    public void Start()
+    {
+        var pluginInfo = new SkateboardPluginDefinition();
+        Logger(pluginInfo.Name + " by " + pluginInfo.Author + " has started.");
+    }
+
+    private void Logger(string message) {
+        var pluginInfo = new SkateboardPluginDefinition();
+        var CYAN = "\u001b[34m";
+        var WHITE = "\u001b[37m";
+        Console.WriteLine(WHITE + "[" + CYAN + pluginInfo.Name + WHITE + "] " + message);    }
+
+}
+
+
+public class SkateboardModule : IChatCommand
+{
+    public string Key => "skateboard";
+
+    public string PermissionRequired => "skateboard_effect_plugin";
+
+    public string Parameters => "";
+
+    public string Description => "Enables the skateboard effect.";
+
+    public async void Execute(GameClient session, Room room, string[] @params)
+    {
+        var skateboardEffectID = 71;
+        session.GetHabbo().Effects().ApplyEffect(skateboardEffectID);
+    }
+}
---
+++ SkateboardPluginDefinition.cs
@@ -0,0 +1,12 @@
+﻿using Plus.Plugins;
+
+namespace SkateboardPlugin
+{
+    public class SkateboardPluginDefinition : IPluginDefinition
+    {
+        public string Name => "Skateboard Plugin";
+        public string Author => "Harb#9937";
+        public Version Version => new(1, 0, 0);
+        public Type PluginClass => typeof(SkateboardPlugin);
+    }
+}
